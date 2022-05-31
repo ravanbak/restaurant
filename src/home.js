6f0105994edaf48e2183fc1eed52fc0ddc242451
David Ravanbakhsh <ravanbak@gmail.com>
2022-05-31 15:36:40 -0700

    Adjust home tab text and styling

--- src/home.js
+++ src/home.js
@@ -1,3 +1,4 @@
+import { text } from '@fortawesome/fontawesome-svg-core';
 import beach from './assets/beach.jpg';
 
 function getHomeContent() {
@@ -12,9 +13,17 @@ function getHomeContent() {
     img.alt = 'A beach in Barbados'
     container.appendChild(img);
     
+    const textContainer = document.createElement('div');
+
     const h1 = document.createElement('h1');
-    h1.textContent = "Located in the heart of Barbados (not pictured here - this is some other part of Barbados)";
-    container.appendChild(h1);
+    h1.textContent = "Located in the heart of Barbados"
+    textContainer.appendChild(h1);
+
+    const h2 = document.createElement('h3');
+    h2.textContent = "(not pictured here - this is some other part of Barbados)";
+    textContainer.appendChild(h2);
+
+    container.appendChild(textContainer);
 
     home.appendChild(container);
 
