089891f9af893c6adaddba43970fb5cb8a5221df
David Ravanbakhsh <ravanbak@gmail.com>
2022-06-02 10:32:28 -0700

    Initial dist subtree commit

--- src/menu.js
+++ src/menu.js
@@ -1,86 +1,15 @@
 import "./menu.css";
 import { addElement } from "./loadpage";
-
-const appetizers = [
-    {
-        name: 'Fried Plantains',
-        description: 'Plantains that have been fried. In a pan.',
-        price: 4.99
-    },
-    {
-        name: 'Fish Cakes',
-        description: 'Cakes. Made out of fish.',
-        price: 6.99
-    },
-    {
-        name: 'Conkies',
-        description: 'No idea what these actually are. Probably delicious, though.',
-        price: 5.99
-    },
-    {
-        name: 'Jug-Jug',
-        description: 'Not sure about this one, either. Maybe two jugs?',
-        price: 7.99
-    },
-    {
-        name: 'Rice and Peas',
-        description: 'Rice. With peas.',
-        price: 5.49
-    },
-];
-
-const mains = [
-    {
-        name: 'Cou-cou',
-        description: 'The national dish of Barbados when eaten with flying fish.',
-        price: 18.99
-    },
-    {
-        name: 'Jug-Jug',
-        description: 'This again. Still don\'t know what it is.',
-        price: 18.99
-    },
-    {
-        name: 'Fried Flying Fish',
-        description: 'Fresh flying fish fillets, fried frantically for five fortnights. Featuring fashionably fat french fries.',
-        price: 18.99
-    }
-];
-
-const desserts = [
-    {
-        name: 'Bajan Black Cake',
-        description: 'Just read the name.',
-        price: 14.99
-    },
-    {
-        name: 'Corn Pie',
-        description: 'This has onion in it, yet it\'s listed as a dessert. You can\'t explain that.',
-        price: 14.99
-    }
-];
-
-const beverages = [
-    {
-        name: 'Water',
-        description: 'Only the finest hydrogen and oxygen atoms, combined in the perfect ratio of 2:1.',
-        price: 0.00
-    },
-    {
-        name: 'Paw-Paw and Coconut Drink',
-        description: 'We take a paw and combine it with another paw, for a total of two paws, then we throw in some coconut.',
-        price: 3.99
-    }
-];
+import menu from "./data/menu.json";
 
 function getMenuContent() {
     const menuContent = document.createElement('div');
     menuContent.classList.add('tab-content');
     
-    addMenuItems(menuContent, 'Appetizers', appetizers);
-    addMenuItems(menuContent, 'Main Courses', mains);
-    addMenuItems(menuContent, 'Desserts', desserts);
-    addMenuItems(menuContent, 'Beverages', beverages);
+    addMenuItems(menuContent, 'Appetizers', menu.appetizers);
+    addMenuItems(menuContent, 'Main Courses', menu.mains);
+    addMenuItems(menuContent, 'Desserts', menu.desserts);
+    addMenuItems(menuContent, 'Beverages', menu.beverages);
 
     return menuContent;
 }
